aa267723aaf024c79d6d2b0ce770a9623a4d1885
WissalBougataia <bougataiawissal@gmail.com>
2025-11-28 19:18:34 +0100

    Resolved merge conflicts and kept my local code

--- campuslink-laravel11/app/Http/Controllers/LikeController.php
+++ campuslink-laravel11/app/Http/Controllers/LikeController.php
@@ -9,15 +9,9 @@ class LikeController extends Controller
 {
     public function toggle(Request $request)
     {
-<<<<<<< HEAD
         $request->validate([
             'announcement_id' => 'required|integer|exists:announcements,id'
         ]);
-=======
-$request->validate([
-    'announcement_id' => 'required|integer|exists:announcements,id'
-]);
->>>>>>> 51dfc8b9df3a6d096f5476827fc51edbb9a4367a
 
         $user = $request->user();
         $announcementId = $request->announcement_id;
@@ -28,7 +22,6 @@ $request->validate([
             $like->delete();
             $liked = false;
         } else {
-<<<<<<< HEAD
             $newLike = $user->likes()->create(['announcement_id' => $announcementId]);
             $liked = true;
 
@@ -43,15 +36,6 @@ $request->validate([
         $announcement = Announcement::with(['user', 'likes.user'])
             ->withCount('likes')
             ->find($announcementId);
-=======
-            $user->likes()->create(['announcement_id' => $announcementId]);
-            $liked = true;
-        }
-
-  $announcement = Announcement::with(['user', 'likes.user'])
-    ->withCount('likes')
-    ->find($announcementId);
->>>>>>> 51dfc8b9df3a6d096f5476827fc51edbb9a4367a
 
         return response()->json([
             'liked' => $liked,
